f97fe52c6b1ac33141ecb39eda2e107df8fe4541
ws0671 <ws0671@naver.com>
2022-11-03 00:13:05 +0900

    3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import React, { useState } from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
-  const [value, setValue] = useState("");
-  // 이러한 React.FormEvent<HTMLInputElement> 같은 것은 혼자 알아내서 적기 힘들다. 따라서
-  // 구글링 또는 docs를 찾아본다.
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // 이 이상한 형태는 es6문법이다. 코드 바깥에서 설명하도록 하겠다.
-    // js에서는 e.target을 주로 썼다면 ts에서는 e.currentTarget을 쓴다고한다.
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 이부분은 기존에 사용하던대로 쓰면된다. */}
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
