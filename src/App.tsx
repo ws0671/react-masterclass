a073f70e99c8125c55bf66641029c25f203267a5
ws0671 <ws0671@naver.com>
2022-11-02 23:41:11 +0900

    3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  // 이러한 React.FormEvent<HTMLInputElement> 같은 것은 혼자 알아내서 적기 힘들다. 따라서
+  // 구글링 또는 docs를 찾아본다.
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // 이 이상한 형태는 es6문법이다. 코드 바깥에서 설명하도록 하겠다.
+    // js에서는 e.target을 주로 썼다면 ts에서는 e.currentTarget을 쓴다고한다.
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal"></Circle>
-      <Circle text="im here" bgColor="tomato"></Circle>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 interface ContainerProps {
   bgColor: string;
@@ -15,10 +16,11 @@ const Container = styled.div<ContainerProps>`
 
 interface CircleProps {
   bgColor: string;
+  // 이름뒤에?를 붙여서 optional로 만들어줌.(선택적인. 즉, 있어도되고 없어도 되는 값으로 만듦. 필수요소가 아니게됨.)
   borderColor?: string;
   text?: string;
 }
-
+// 밑에줄에 text='default text'는 js ES6문법으로, 기본값을 설정할때 사용한다.
 function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
   return (
     <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
