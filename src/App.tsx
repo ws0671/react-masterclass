4d43e7ee834e5a97fff0e34ca45da364200ef74b
ws0671 <ws0671@naver.com>
2022-11-07 14:19:39 +0900

    5.0 Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import styled from "styled-components";
+import Router from "./Router";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
 function App() {
-  return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme, lightTheme } from "./theme";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     {/* 이부분은 기존에 사용하던대로 쓰면된다. */}
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
